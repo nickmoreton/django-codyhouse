94b74c2f4e04a5aaca792d0e2cdd01b513abd2d5
Nick Moreton <nickmoreton@me.com>
2020-08-16 17:52:41 +0100

    Update setup_django.sh

--- setup_django.sh
+++ setup_django.sh
@@ -2,6 +2,5 @@ pip install pip --upgrade \
 && pip install django \
 && django-admin startproject config . \
 && ./manage.py migrate \
-&& ./manage.py collectstatic \
 && rm get_frontend.sh \
 && rm setup_django.sh
