d439d381036a4de9cea8dcc0273b59be4b61d2cd
Nick Moreton <nickmoreton@me.com>
2020-08-16 17:38:13 +0100

    Update setup_wagtail.sh

--- setup_wagtail.sh
+++ setup_wagtail.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 pip install pip --upgrade \
-&& pip install wagtail==2.9.3 \
-&& wagtail start config . \
+&& pip install django \
+&& dango-admin startproject config . \
 && ./manage.py migrate \
 && ./manage.py collectstatic \
-&& rm Dockerfile \
-&& rm requirements.txt \
 && chmod +x get_frontend.sh \
 && rm get_frontend.sh \
-&& rm setup_wagtail.sh
+&& rm setup_django.sh
