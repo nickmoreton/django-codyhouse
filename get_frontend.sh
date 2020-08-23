2024ce31eeaa635a06ca4994cee5dd52f09eb2dd
Nick Moreton <nickmoreton@me.com>
2020-08-23 21:38:31 +0100

    Update get_frontend.sh

--- get_frontend.sh
+++ get_frontend.sh
@@ -2,7 +2,10 @@
 
 # gets codyhouse-framework and extracts to _theme
 wget https://github.com/CodyHouse/codyhouse-framework/archive/master.zip && unzip master.zip \
+&& rm codyhouse-framework-master/README.md \
+&& rm codyhouse-framework-master/LICENSE.md \
+&& rm codyhouse-framework-master/.gitignore \
 && mv codyhouse-framework-master/** . && mv main _theme \
-&& rm -R codyhouse-framework-master && rm master.zip && rm LICENSE.md && rm gulpfile.js \
+&& rm -R codyhouse-framework-master && rm master.zip && rm gulpfile.js \
 && mv sample.gulpfile.js gulpfile.js \
 && npm install
